06844c372c1e244afdf0ffba8e7bf925649fac8b
Jorge Carcamo <jcarcamo@nectia.com>
2022-05-06 22:18:57 -0400

    🐛 Add eslint-disable-next-line react-hooks/exhaustive-deps

--- src/hooks/useFecth.js
+++ src/hooks/useFecth.js
@@ -22,6 +22,7 @@ const useFecth = () => {
       }, 1000);
       return () => clearTimeout(timeoutId);
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [data.plate]);
 
   return { data, setData };
